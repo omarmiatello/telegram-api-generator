51541445477e97a2ccfa8660f631e7102196dd59
omarmiatello <jackl85@gmail.com>
2019-02-23 14:49:51 +0100

    Refactor example

--- src/main/kotlin/TelegramApiGenerator.kt
+++ src/main/kotlin/TelegramApiGenerator.kt
@@ -4,19 +4,21 @@ import java.io.File
 
 fun main() {
     val sections = webpageToSection(Jsoup.parse(File("data/telegramapi.html").readText()))
-    sections.forEach {
-        println("\n\n## ${it.name}")
-        if (it.types.isNotEmpty()) {
-            println("\n### Data Types")
-            it.types.forEach {
-                println("    ${it.name}(${it.fields.map { "${it.name}: ${it.type}" }.joinToString()})")
-            }
+    sections.toReadmeExample()
+}
+
+private fun List<Section>.toReadmeExample() = forEach {
+    println("\n\n## ${it.name}")
+    if (it.types.isNotEmpty()) {
+        println("\n### Data Types")
+        it.types.forEach {
+            println("    ${it.name}(${it.fields.map { "${it.name}: ${it.type}" }.joinToString()})")
         }
-        if (it.methods.isNotEmpty()) {
-            println("\n### Methods")
-            it.methods.forEach {
-                println("    ${it.name}(${it.parameter.map { "${it.name}: ${it.type}" }.joinToString()})")
-            }
+    }
+    if (it.methods.isNotEmpty()) {
+        println("\n### Methods")
+        it.methods.forEach {
+            println("    ${it.name}(${it.parameter.map { "${it.name}: ${it.type}" }.joinToString()})")
         }
     }
 }
