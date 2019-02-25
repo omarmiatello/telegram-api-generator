d14835831db88bb07e1d42c539ccc8e89f6e6b22
omarmiatello <jackl85@gmail.com>
2019-02-25 12:11:44 +0100

    Update telegram.json example

--- src/main/kotlin/GeneratorJson.kt
+++ src/main/kotlin/GeneratorJson.kt
@@ -2,7 +2,7 @@ import com.google.gson.Gson
 import java.io.File
 
 fun List<DocSection>.toJson() {
-    File("out/example.json").writer().apply {
+    File("out/telegram.json").writer().apply {
         write(Gson().newBuilder().setPrettyPrinting().create().toJson(this@toJson))
         close()
     }
